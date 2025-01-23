bb26624e3259d47068b64c17a25b3ec054337815
Dilshani-Prabodha <dilshaniprabo@gmail.com>
2025-01-23 22:00:24 +0530

    header color change

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -22,7 +22,7 @@
   ```
 -->
 <div class="min-h-full">
-  <nav class="bg-green-800">
+  <nav class="bg-green-600">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div class="flex h-16 items-center justify-between">
         <div class="flex items-center">
